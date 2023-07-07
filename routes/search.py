9e03208450850e5cde6bc3e0868150d542209eb0
Daniel Tam <daniel@danieltam.net>
2023-07-07 16:57:41 -0500

    create serverless search route

--- routes/search.py
+++ routes/search.py
@@ -1,6 +1,25 @@
 # For testing
 import json
 
+# Helper function
+def return_bad():
+    """
+    Returns a message stating it was a bad request
+
+    Returns:
+        JSON:
+            statusCode: 400,
+            headers: Content-Type text/plain
+            body: 400: Bad request
+    """
+    return {
+        'statusCode': 400,
+        'headers': {
+            'Content-Type': 'text/plain'
+        },
+        'body': '400: Bad request'
+    }
+
 # Routes
 def lambda_handler(event, context):
     """
@@ -11,12 +30,35 @@ def lambda_handler(event, context):
             titles: the titles of animes
     """
 
+    # Ensure there's something in the body
+    if not event['body']:
+        return return_bad()
+    
+    # Load the query
+    query = json.loads(event['body'])
+    search_item = query['query']
+
+    # Ensure that the query is at least 3 characters
+    if len(search_item) < 3 or len(search_item) > 15:
+        return return_bad()
+        
+    # For testing, open file directly and load it
+    f = open('./parsed-anime-list-mini.json')
+    data = json.load(f)
+
+    # Keep track of titles that are possible
+    anime_list = {"titles": []}
+
+    # Cycle through list to find an include
+    for item in data:
+        if search_item.lower() in item['title'].lower():
+            anime_list['titles'].append(item['title'])
+
+    # Return the list of animes
     return {
         'statusCode': 200,
         'headers': {
             'Content-Type': 'application/json'
         },
-        'body': json.dumps({
-            '':''
-            })
+        'body': json.dumps(anime_list)
     }
