dc89450f2eb1d2fd8063e8c70a4de02cfb8230d0
Daniel Tam <omgdanieltam@gmail.com>
2023-09-28 00:43:52 -0500

    fix return headers

--- routes/time/time.py
+++ routes/time/time.py
@@ -40,6 +40,9 @@ def lambda_handler(event, context):
     return {
         'statusCode': 200,
         'headers': {
+            'Access-Control-Allow-Headers': 'Content-Type',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'GET',
             'Content-Type': 'application/json'
         },
         'body': json.dumps({
